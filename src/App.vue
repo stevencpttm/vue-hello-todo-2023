9aaedbf22cd1e7d949f439920c3767db9c026b82
stevencpttm <56670584+stevencpttm@users.noreply.github.com>
2023-04-29 16:39:43 +0800

    todo example

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Todos</h1>
+
+  <ul>
+    <li
+      :class="{ completed: todo.completed }"
+      v-for="todo in todos"
+      :key="todo.id"
+      @click="toggleItemState(todo.id)"
+    >
+      {{ todo.title }}
+    </li>
+  </ul>
+
+  <input
+    type="text"
+    v-model="item"
+    placeholder="Todo Item"
+    @keyup.enter="addItem"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      item: "",
+      todos: [
+        {
+          id: 1,
+          title: "Item A",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Item B",
+          completed: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.item === "") return;
+
+      this.todos.push({
+        id: this.todos[this.todos.length - 1].id + 1,
+        title: this.item,
+        completed: false,
+      });
+      this.item = "";
+    },
+    toggleItemState(itemId) {
+      const item = this.todos.find((todo) => todo.id === itemId);
+      item.completed = !item.completed;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+input[type="text"] {
+  display: block;
+  width: 100%;
+  font-size: 32px;
+}
+.completed {
+  color: #ccc;
+  text-decoration: line-through;
 }
 </style>
